7e7de2b0252561241cf5ecd728a1871a1f03873d
Oliver150515 <oliver.luis.garcia@protonmail.com>
2021-11-30 15:11:43 -0400

    Ejemplo de test
    un breve ejemplo de como hacer test con JEST

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,20 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+// PARA CORRER LOS TEST SE UNA: npm run test:unit
+// DOCUMENTACION: https://jestjs.io/
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+// NOMBRE DEL COMPONENTE
+describe('example Component', () => {
+  // NOMBRE DE LA PRUEBA
+  test('Debe de ser mayor a 10', () => {
+
+    //Arreglar
+    let value = 5;
+
+    // estimulo
+    value = value + 5
+
+    // EJECUCION DE LAS PRUEBAS O VALIDADORES
+    expect(value).toBeGreaterThan(10)
+
   })
+
 })
